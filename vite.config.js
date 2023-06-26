b192708ec78c5867b2b1d908bee2d4dd8ced15ed
Victor Zuluaga <zrvictor@outlook.com>
2023-06-25 19:35:26 -0500

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: path.resolve(__dirname, 'build')
+  },
   plugins: [react()],
 })

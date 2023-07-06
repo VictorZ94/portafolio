ab02920f4cca13c78516937e2e193d2313821b4b
Victor Zuluaga <zrvictor@outlook.com>
2023-07-06 12:46:53 -0500

    updated about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,7 +20,7 @@ const AboutSection = () => {
           <span className="position-relative fw-light">a brief intro about me</span>
         </p>
         <div className="description px-2 pt-4 px-md-5">
-          <p>I'm a React developer, with experience turning ideas into functional web products. Moreover, I have knowledge in Back-end with Python and Django, although I am specializing in Front-end.</p>
+          <p>I'm a web developer, with experience turning ideas into functional web products. I have been seasoned working with Python and Django in Back-end alongside with React and Javascript in Front-end</p>
           <p>I have worked for several startups, always maintaining as a priority the idea that it is better to do things well than to do them perfect. I love to work iteratively, constantly improving the product through feedback and new ideas.</p>
           <p>If you need help in the development of your web project, I will be happy to work with you and help you bring your ideas to reality.</p>
         </div>

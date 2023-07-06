86f309886c10c9d764e29f3f67bca8b482b22fa5
Victor Zuluaga <zrvictor@outlook.com>
2023-07-06 12:34:08 -0500

    improve skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import { Button, Col, Container, Row } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next';
-
-// @icons
-import HtmlIcon from "../assets/icons/html5-icon.svg"
-import CssIcon from "../assets/icons/css3-icon.svg"
-import ReactIcon from "../assets/icons/reactjs-icon.svg"
-import JsIcon from "../assets/icons/js-icon.svg"
-import PythonIcon from "../assets/icons/python-icon.svg"
-import MongoIcon from "../assets/icons/mongodb-icon.svg"
-import DjangoIcon from "../assets/icons/django-icon.svg"
-import ApiIcon from "../assets/icons/api-icon.png"
-import JsonIcon from "../assets/icons/json-icon.svg"
-import GrapqhqlIcon from "../assets/icons/graphql-icon.svg"
-
+import iconSkills from "../data/skills.js"
 
 const Skills = () => {
   const [t] = useTranslation("global");
+  const [skills, setSkills] = useState("all");
+  const [filteredSkills, setFilteredSkills] = useState([]);
+
+  useEffect(() => {
+    if (skills !== "all") {
+      setFilteredSkills(iconSkills.filter(item => item.target.includes(skills)))
+    } else {
+      setFilteredSkills(iconSkills)
+    }
+  }, [skills]) 
+
+  console.log(filteredSkills)
 
   return (
     <section id={t("nav.skills").toLowerCase()}>
@@ -29,48 +29,33 @@ const Skills = () => {
           <span className="position-relative fw-light">tech stack</span>
         </p>
         <Row>
-          <Col sm={12} lg={6} className="my-3">
-            <h5>Frontend development</h5>
-            <div>
-              <span>
-                <img src={HtmlIcon} alt="Html5 icon" width={48}/>
-              </span>
-              <span>
-                <img src={CssIcon} alt="Html5 icon" width={48}/>
-              </span>
-              <span>
-                <img src={ReactIcon} alt="Html5 icon" width={48}/>
-              </span>
-              <span>
-                <img src={JsIcon} alt="Html5 icon" width={48}/>
-              </span>
-            </div>
-          </Col>
-          <Col sm={12} lg={6} className="my-3">
-            <h5>Back-end development</h5>
-            <div>
-              <span>
-                <img src={PythonIcon} alt="Html5 icon" width={48}/>
-              </span>
-              <span>
-                <img src={DjangoIcon} alt="Html5 icon" width={48}/>
-              </span>
-              <span>
-                <img src={MongoIcon} alt="Html5 icon" width={48}/>
-              </span>
-              <span>
-                <img src={ApiIcon} alt="Html5 icon" width={48}/>
-              </span>
-              <span>
-                <img src={JsonIcon} alt="Html5 icon" width={48}/>
-              </span>
-              <span>
-                <img src={GrapqhqlIcon} alt="Html5 icon" width={98}/>
-              </span>
-            </div>
+          <Col>
+              <Button
+                onClick={() => setSkills("all")}
+                className='my-2 me-3 outline-custom'>
+                  All
+              </Button>
+              <Button
+                onClick={() => setSkills("back-end")}
+                className='my-2 me-3 outline-custom'>
+                  Backend
+              </Button>
+              <Button
+                onClick={() => setSkills("front-end")}
+                className='outline-custom'>
+                  Frontend
+              </Button>
           </Col>
         </Row>
-        <p className='my-2 text-secondary'>I'm willing to learn other skills as required.</p>
+        <Row className='mt-5'>
+          {filteredSkills.sort((a, b) => a.order - b.order).map(icon => (
+            <Col key={icon.alt} md={6} lg={4} xl={3} className='p-5 table-color'>
+              <div className='icon-skill'>
+                <img src={icon.image} alt={icon.alt} height={icon.height}/>
+              </div>
+            </Col>
+          ))}
+        </Row>
       </Container>
     </section>
   )

a06b7532ab7441e16f4ad89e40c800b35c1d5a95
Victor Zuluaga <zrvictor@outlook.com>
2023-07-06 06:46:51 -0500

    redesign blog section

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container';
 import { useTranslation } from "react-i18next";
-import { Card, Col, Row } from 'react-bootstrap';
 import ImgBlogPost from "../assets/intro-bg4.jpg";
 
 const BlogSection = () => {
@@ -14,44 +13,21 @@ const BlogSection = () => {
         <p>
           <span className="position-relative fw-light">Latest post</span>
         </p>
-        <Row className='mt-5'>
-          <Col md={6} lg={4} className='mb-3'>
-            <Card>
-              <Card.Img variant="top" src={ImgBlogPost} alt="Blog post image"/>
-              <Card.Body>
-                <Card.Title>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md={6} lg={4} className='mb-3'>
-            <Card>
-              <Card.Img variant="top" src={ImgBlogPost} alt="Blog post image"/>
-              <Card.Body>
-                <Card.Title>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md={6} lg={4}  className='mb-3'>
-            <Card>
-              <Card.Img variant="top" src={ImgBlogPost} alt="Blog post image"/>
-              <Card.Body>
-                <Card.Title>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
+        <div class="card mx-sm-2 mx-md-5 mt-5 mb-3">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src={ImgBlogPost} class="img-fluid rounded-start" alt="..." />
+            </div>
+            <div class="col-md-8 text-start p-4">
+              <div class="card-body">
+                <h3 class="card-title fw-light">Card title</h3>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                <a href="#">Read More!</a>
+              </div>
+            </div>
+          </div>
+        </div>
       </Container>
     </section>
   )

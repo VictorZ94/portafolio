ae03f4e594bb4d5c2eb85737ba5665f6f67ea838
Victor Zuluaga <zrvictor@outlook.com>
2023-07-06 14:56:40 -0500

    updated blog section

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container';
 import { useTranslation } from "react-i18next";
-import ImgBlogPost from "../assets/intro-bg4.jpg";
+import blogPost from "../data/blogs.js"
+import CardBlog from './CardBlog';
 
 const BlogSection = () => {
   const [t] = useTranslation("global");
@@ -13,21 +14,9 @@ const BlogSection = () => {
         <p>
           <span className="position-relative fw-light">Latest post</span>
         </p>
-        <div class="card mx-sm-2 mx-md-5 mt-5 mb-3">
-          <div class="row g-0">
-            <div class="col-md-4">
-              <img src={ImgBlogPost} class="img-fluid rounded-start" alt="..." />
-            </div>
-            <div class="col-md-8 text-start p-4">
-              <div class="card-body">
-                <h3 class="card-title fw-light">Card title</h3>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                <a href="#">Read More!</a>
-              </div>
-            </div>
-          </div>
-        </div>
+        {blogPost.map(blog => (
+          <CardBlog key={blog.blogId} {...blog} />
+        ))}
       </Container>
     </section>
   )

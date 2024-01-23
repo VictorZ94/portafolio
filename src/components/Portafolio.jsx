1b0d1e645e00aba3bac834e012209068bd2219f8
Victor Zuluaga <zrvictor@outlook.com>
2024-01-22 20:56:27 -0500

    indicadores colombianos

--- src/components/Portafolio.jsx
+++ src/components/Portafolio.jsx
@@ -1,10 +1,10 @@
-import React from "react";
 import Container from "react-bootstrap/Container";
 import { useTranslation } from "react-i18next";
 import { Link, Search } from "react-feather";
 import previewSmilingSchool from "../assets/preview-smiling-school.png";
 import previewMoviesSeach from "../assets/buscador-pelis.png";
 import Wallet from "../assets/wallet.png";
+import Indicadores from "../assets/indicadoresco.png";
 
 const PortafolioSection = () => {
   const [t] = useTranslation("global");
@@ -21,28 +21,53 @@ const PortafolioSection = () => {
         <div className="projects mt-5">
           <div className="single-project">
             <img
-              src={previewSmilingSchool}
+              src={Indicadores}
               alt="First project"
               width={280}
               height={350}
             />
             <div className="project-body">
-              <h2>Smiling School</h2>
-              <p>Front-end project</p>
+              <h2>Indicadores Colombia</h2>
+              <p>Indicadores colombianos más importantes</p>
               <p>
                 <a
-                  href="https://github.com/VictorZ94/Smilling-school"
+                  href="https://github.com/VictorZ94/IndicadoresCO"
                   target="_blank"
                   rel="noreferrer"
-                  title="Repositorio"
+                  title="Github link - Indicadores Colombia"
                 >
                   <Search color="white" width={18} />
                 </a>
                 <a
-                  href="https://victorz94.github.io/Smilling-school/homepage.html"
+                  href="https://indicadoresco-65f4c3b60b35.herokuapp.com/docs/"
                   target="_blank"
                   rel="noreferrer"
-                  title="Smiling School website"
+                  title="Indicadores Colombia"
+                >
+                  <Link color="white" width={18} />
+                </a>
+              </p>
+            </div>
+          </div>
+          <div className="single-project">
+            <img src={Wallet} alt="First project" width={280} height={350} />
+            <div className="project-body">
+              <h2>Digital wallet</h2>
+              <p>Django Backend & React Frontend</p>
+              <p>
+                <a
+                  href="https://github.com/No-Country/c13-30-t-python"
+                  target="_blank"
+                  rel="noreferrer"
+                  title="Github link - Chat mock ChatGPT"
+                >
+                  <Search color="white" width={18} />
+                </a>
+                <a
+                  href="https://digital-wallet.victorzuluaga.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                  title="Chat mock ChatGPT"
                 >
                   <Link color="white" width={18} />
                 </a>
@@ -80,24 +105,29 @@ const PortafolioSection = () => {
             </div>
           </div>
           <div className="single-project">
-            <img src={Wallet} alt="First project" width={280} height={350} />
+            <img
+              src={previewSmilingSchool}
+              alt="First project"
+              width={280}
+              height={350}
+            />
             <div className="project-body">
-              <h2>Digital wallet</h2>
-              <p>Django Backend & React Frontend</p>
+              <h2>Smiling School</h2>
+              <p>Front-end project</p>
               <p>
                 <a
-                  href="https://github.com/No-Country/c13-30-t-python"
+                  href="https://github.com/VictorZ94/Smilling-school"
                   target="_blank"
                   rel="noreferrer"
-                  title="Github link - Chat mock ChatGPT"
+                  title="Repositorio"
                 >
                   <Search color="white" width={18} />
                 </a>
                 <a
-                  href="https://digital-wallet.victorzuluaga.com/"
+                  href="https://victorz94.github.io/Smilling-school/homepage.html"
                   target="_blank"
                   rel="noreferrer"
-                  title="Chat mock ChatGPT"
+                  title="Smiling School website"
                 >
                   <Link color="white" width={18} />
                 </a>
